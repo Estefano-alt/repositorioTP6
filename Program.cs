f870b02983605ee8493c23e725048fa980b2ca09
Estefano <estefanoybarra.18@gmail.com>
2026-05-25 18:29:05 -0300

    fourth commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+Console.WriteLine("valor de b:"+b);*/
